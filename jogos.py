a7e41c434a5ed6866a0da7ac591ea8821af9e643
Augusto Madeira Splett (Tinho) <augusto.splett@gmail.com>
2022-03-30 21:44:59 -0300

    Implementado lógica de seleção de jogo. Implementado estrutura básica do jogo da forca.

--- jogos.py
+++ jogos.py
@@ -1,1 +1,28 @@
 import adivinhacao
+import forca
+import os
+
+
+def cls():
+    os.system('cls' if os.name == 'nt' else 'clear')
+
+
+print("**************************************")
+print("Bem vindo ao menu de seleção de jogos!")
+print("**************************************")
+print( "(1) Jogo de Adivinhação")
+print( "(2) Jogo da Forca")
+print("**************************************")
+
+selected_game = int(input("Digite o número do jogo desejado: "))
+
+cls()
+
+if selected_game == 1:
+    adivinhacao.play()
+else:
+    forca.play()
+
+
+
+
